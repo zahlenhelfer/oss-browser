763a902500471d4d1187a609f71d2e7cae61635f
Marcus Ross <marcus.ross@zahlenhelfer.de>
2018-09-18 10:30:00 +0200

    Update index.js for new translation

--- node/i18n/index.js
+++ node/i18n/index.js
@@ -2,5 +2,6 @@
 module.exports = {
   'zh-CN': {label: '简体中文', content: require('./zh-CN') },
   'en-US': {label: 'English', content: require('./en-US') },
+  'de-DE': {label: 'Deutsch', content: require('./de-DE') },
   'ja-JP': {label: '日本語', content: require('./ja-JP') },
 };
